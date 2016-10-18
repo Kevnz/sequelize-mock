5458f83ac87d9c01892dccc051e2f84287df2a74
Kevin Isom <kevin.isom@gmail.com>
2016-10-18 16:08:04 +1300

    Data Types and importing

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var bluebird = require('bluebird'),
-	Model = require('./model');
+    Model = require('./model');
 
 function Sequelize() {
-	
+
 }
 
 Sequelize.version = require('../package.json').version;
@@ -46,35 +46,85 @@ Sequelize.GEOGRAPHY = function () {};
 
 // QUERY TYPES
 Sequelize.QueryTypes = {
-	// FROM https://github.com/sequelize/sequelize/blob/master/lib/query-types.js
-	SELECT: 'SELECT',
-	INSERT: 'INSERT',
-	UPDATE: 'UPDATE',
-	BULKUPDATE: 'BULKUPDATE',
-	BULKDELETE: 'BULKDELETE',
-	DELETE: 'DELETE',
-	UPSERT: 'UPSERT',
-	VERSION: 'VERSION',
-	SHOWTABLES: 'SHOWTABLES',
-	SHOWINDEXES: 'SHOWINDEXES',
-	DESCRIBE: 'DESCRIBE',
-	RAW: 'RAW',
-	FOREIGNKEYS: 'FOREIGNKEYS',
+    // FROM https://github.com/sequelize/sequelize/blob/master/lib/query-types.js
+    SELECT: 'SELECT',
+    INSERT: 'INSERT',
+    UPDATE: 'UPDATE',
+    BULKUPDATE: 'BULKUPDATE',
+    BULKDELETE: 'BULKDELETE',
+    DELETE: 'DELETE',
+    UPSERT: 'UPSERT',
+    VERSION: 'VERSION',
+    SHOWTABLES: 'SHOWTABLES',
+    SHOWINDEXES: 'SHOWINDEXES',
+    DESCRIBE: 'DESCRIBE',
+    RAW: 'RAW',
+    FOREIGNKEYS: 'FOREIGNKEYS',
 };
 
 Sequelize.prototype.define = function (name, obj, opts) {
-	return new Model(name, obj, opts);
+    return new Model(name, obj, opts);
 };
 Sequelize.prototype.query = function () {
-	return bluebird.reject(new Error('This function requires test specific configuration as it is too broad to generalize'));
+    return bluebird.reject(new Error('This function requires test specific configuration as it is too broad to generalize'));
 };
 Sequelize.prototype.transaction = function (fn) {
-	return new bluebird(function (resolve, reject) {
-		return fn({}).then(resolve, reject);
-	});
+    return new bluebird(function (resolve, reject) {
+        return fn({}).then(resolve, reject);
+    });
 };
 Sequelize.prototype.literal = function (arg) {
-	return arg;
+    return arg;
 };
 
+Sequelize.prototype.DataTypes = {
+    ABSTRACT: function () {},
+    STRING: function () {},
+    CHAR: function () {},
+    TEXT: function () {},
+    NUMBER: function () {},
+    INTEGER: function () {},
+    BIGINT: function () {},
+    FLOAT: function () {},
+    TIME: function () {},
+    DATE: function () {},
+    DATEONLY: function () {},
+    BOOLEAN: function () {},
+    NOW: function () {},
+    BLOB: function () {},
+    DECIMAL: function () {},
+    NUMERIC: function () {},
+    UUID: function () {},
+    UUIDV1: function () {},
+    UUIDV4: function () {},
+    HSTORE: function () {},
+    JSON: function () {},
+    JSONB: function () {},
+    VIRTUAL: function () {},
+    ARRAY: function () {},
+    NONE: function () {},
+    ENUM: function () {},
+    RANGE: function () {},
+    REAL: function () {},
+    DOUBLE: function () {},
+    'DOUBLE PRECISION': function () {},
+    GEOMETRY: function () {},
+    GEOGRAPHY: function () {}
+};
+
+Sequelize.prototype.importCache = {
+
+}
+Sequelize.prototype.import = function(modelPath) {
+    if (!this.importCache[modelPath]) {
+        var defineCall = arguments.length > 1 ? arguments[1] : require(modelPath);
+        if (typeof defineCall === 'object') {
+        // ES6 module compatability
+            defineCall = defineCall.default;
+        }
+        this.importCache[modelPath] = defineCall(this, this.DataTypes);
+    }
+
+    return this.importCache[modelPath];
+};
 module.exports = Sequelize;
